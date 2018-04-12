0252ebc3fa29327226a84bbe7517736f4f0250d3
BenjaminApa <sb.tempub@gmail.com>
2018-04-12 10:08:42 +0200

    First commit

--- src/Back/Tuple.java
+++ src/Back/Tuple.java
@@ -1,5 +1,47 @@
 package Back;
 
-public class Tuple {
+public class Tuple<X, Y> {
+
+	private int c1;
+	private int c2;
+
+	public Tuple(int l, int r) {
+		this.c1 = l;
+		this.c2 = r;
+	}
+
+	public int sommeTuple() {
+		return c1 + c2;
+	}
+
+	public boolean isSpare() {
+		if (c1 + c2 == 10 && c1 != 10) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isStrike() {
+		if (c1 + c2 == 10 && c1 == 10) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getc1() {
+		return c1;
+	}
+
+	public int getc2() {
+		return c2;
+	}
+
+	public void setc1(int l) {
+		this.c1 = l;
+	}
+
+	public void setc2(int r) {
+		this.c2 = r;
+	}
 
 }
--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -1,4 +1,5 @@
 package Tests;
+import junit.framework.*;
 
 public class Test {
 
